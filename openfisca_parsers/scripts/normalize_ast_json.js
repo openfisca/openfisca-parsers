017881a99cc347da18d6b9bb53bcd0b6ab9cec92
Christophe Benz <christophe.benz@gmail.com>
2016-06-02 11:48:50 +0200

    Implement read from stdin

--- openfisca_parsers/scripts/normalize_ast_json.js
+++ openfisca_parsers/scripts/normalize_ast_json.js
@@ -47,13 +47,29 @@ variableForPeriod.define({
   variable
 })
 
-if (process.argv.length < 3 || !process.argv[2]) {
-  throw new Error('Provide a file path')
+function main (fileContent) {
+  const node = JSON.parse(fileContent)
+  const result = normalize(node, arrayOf(variable))
+  console.log(JSON.stringify(result, null, 2))
 }
 
-const filePath = process.argv[2]
-const node = JSON.parse(fs.readFileSync(filePath))
-
-const result = normalize(node, arrayOf(variable))
-
-console.log(JSON.stringify(result, null, 2))
+if (process.argv.length < 3 || !process.argv[2]) {
+  let fileContent = ''
+  process.stdin.setEncoding('utf8')
+  process.stdin.on('readable', () => {
+    var chunk = process.stdin.read()
+    if (chunk !== null) {
+      fileContent += chunk
+    }
+  })
+  process.stdin.on('end', () => {
+    main(fileContent)
+  })
+} else {
+  if (process.argv.length > 3) {
+    throw new Error('Provide a single file path')
+  }
+  const filePath = process.argv[2]
+  const fileContent = fs.readFileSync(filePath)
+  main(fileContent)
+}
