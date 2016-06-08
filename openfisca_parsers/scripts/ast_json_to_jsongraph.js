00e28c4d247634ffbb70a8e9693288e770b37862
Christophe Benz <christophe.benz@gmail.com>
2016-06-08 16:10:02 +0200

    Fix shebangs

--- openfisca_parsers/scripts/ast_json_to_jsongraph.js
+++ openfisca_parsers/scripts/ast_json_to_jsongraph.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
 import ArraySchema from 'normalizr/lib/IterableSchema'
 import EntitySchema from 'normalizr/lib/EntitySchema'
--- openfisca_parsers/scripts/normalize_ast_json.js
+++ openfisca_parsers/scripts/normalize_ast_json.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
 import {normalize, valuesOf} from 'normalizr'
 import read from 'read-file-stdin'
