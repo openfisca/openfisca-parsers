b20b0dfad8349b80e2e6a01663cf796b4281f68c
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-07-11 10:58:54 +0200

    [TO REVERT] Test automatic release

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-Parsers',
-    version = '0.5.1',
+    version = '0.5.1.dev2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
